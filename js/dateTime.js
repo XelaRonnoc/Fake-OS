7e93107a2398f44e82d19c94a65f1c0ecb142f5b
AlexanderMilton47159375 <Alexander.Milton1@students.mq.edu.au>
2023-03-29 16:09:26 +1100

    begun writing time update function

--- js/dateTime.js
+++ js/dateTime.js
@@ -38,3 +38,28 @@ export const updateDateTime = (container) => {
 
     container.appendChild(innerDiv);
 };
+
+const updateTime = () => {
+    const today = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let curHour = date.getHours();
+    let curMin = date.getMinutes();
+    let ampm = "AM";
+    if (curHour > 12) {
+        curHour = curHour - 12;
+        ampm = "PM";
+    }
+    if (curMin < 10) {
+        curMin = "0" + curMin;
+    }
+    if (month < 10) {
+        month = "0" + month;
+    }
+    if (day < 10) {
+        day = "0" + day;
+    }
+
+    // return an object with date and time that can be passed into the divs to update the time
+};
