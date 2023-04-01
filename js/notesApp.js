937602fad748d1b232f639912fcee1c4076cd5f7
AlexanderMilton47159375 <Alexander.Milton1@students.mq.edu.au>
2023-04-01 14:24:19 +1100

    added icon8 references and adjusted some styling for the desktop icons

--- js/notesApp.js
+++ js/notesApp.js
@@ -1,32 +1,55 @@
 import { addIconToFooter, exitApp } from "./globalFunctions.js";
 import { savedElements } from "./formApp.js";
 
+const referenceArray = [
+    '<a target="_blank" href="https://icons8.com/icon/XTwDewtklGCW/reminders">Reminders icon by Icons8</a>',
+    '<a target="_blank" href="https://icons8.com/icon/52562/pictures-folder">Pictures Folder icon by Icons8</a>',
+    '<a target="_blank" href="https://icons8.com/icon/JrInpv4e05vK/windows-notepad">Windows Notepad icon by Icons8</a>',
+    '<a target="_blank" href="https://icons8.com/icon/wUAGUBXx2syB/office-365">Office 365 icon by Icons8</a>',
+    '<a target="_blank" href="https://icons8.com/icon/48184/microsoft-word">Microsoft Word icon by Icons8</a>',
+    '<a target="_blank" href="https://icons8.com/icon/oLry165Qopkd/microsoft-edge">Microsoft Edge icon by Icons8</a>',
+    '<a target="_blank" href="https://icons8.com/icon/5CLSNos6pTuq/doom-logo">Doom Logo icon by Icons8</a>',
+    '<a target="_blank" href="https://icons8.com/icon/44029/music">Music icon by Icons8</a>',
+    '<a target="_blank" href="https://icons8.com/icon/q729jEheSR92/knight">Knight icon by Icons8</a>',
+    '<a target="_blank" href="https://icons8.com/icon/69442/dropbox">Dropbox icon by Icons8</a>',
+    '<a target="_blank" href="https://icons8.com/icon/2a1IfE0f2djh/shop-local">Shop Local icon by Icons8</a>',
+    '<a target="_blank" href="https://icons8.com/icon/44826/clapperboard">Clapperboard icon by Icons8</a>',
+    '<a target="_blank" href="https://icons8.com/icon/43655/restart">Restart icon by Icons8</a>',
+    '<a target="_blank" href="https://icons8.com/icon/43681/protect">Protect icon by Icons8</a>',
+    '<a target="_blank" href="https://icons8.com/icon/44037/plus">Plus icon by Icons8</a>',
+];
+
 export const fillNotesApp = (container) => {
     // creates a new header and para element for each index in saved elements array
 
-    if (savedElements.length === 0) {
-        const placeHolder = document.createElement("h1");
-        placeHolder.classList.add("placeholder");
-        const placeholderText = document.createTextNode(
-            "No Notes have been saved"
-        );
-        placeHolder.appendChild(placeholderText);
-        container.appendChild(placeHolder);
-    } else {
-        for (let i = 0; i < savedElements.length; i++) {
-            const log = document.createElement("div");
-            const header = document.createElement("h1");
-            const headerText = document.createTextNode(savedElements[i][0]);
-            header.appendChild(headerText);
+    const references = document.createElement("div");
+    const refHeader = document.createElement("h1");
+    const refHeaderText = document.createTextNode("References");
+    refHeader.appendChild(refHeaderText);
 
-            const para = document.createElement("p");
-            const paraText = document.createTextNode(savedElements[i][1]);
-            para.appendChild(paraText);
+    const refPara = document.createElement("ul");
+    for (let i = 0; i < referenceArray.length; i++) {
+        refPara.innerHTML += "<li>" + referenceArray[i];
+        ("</li>");
+    }
+    references.appendChild(refHeader);
+    references.appendChild(refPara);
+    container.appendChild(references);
 
-            log.appendChild(header);
-            log.appendChild(para);
-            container.appendChild(log);
-        }
+    for (let i = 0; i < savedElements.length; i++) {
+        const log = document.createElement("div");
+        const header = document.createElement("h1");
+        const headerText = document.createTextNode(savedElements[i][0]);
+        header.appendChild(headerText);
+
+        const para = document.createElement("p");
+        const paraText = document.createTextNode(savedElements[i][1]);
+        para.appendChild(paraText);
+
+        log.appendChild(header);
+        log.appendChild(para);
+        container.appendChild(log);
+        // }
     }
 };
 
